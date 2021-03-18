51a3e308994d9bc9e4fa8558c4af98a5ee10a82b
Rado <rado@dilypse.io>
2021-03-18 20:32:52 +0300

    Fix css

---
+++ getLocalIdent.js
@@ -0,0 +1,40 @@
+import incstr from 'incstr'
+
+// region CSS Scope Minify
+const createUniqueIdGenerator = () => {
+  const index = {}
+
+  const generateNextId = incstr.idGenerator({
+    alphabet: 'abcdefghijklmnopqrstuvwxyz0123456789_-',
+  })
+
+  return (name) => {
+    if (index[name]) {
+      return index[name]
+    }
+
+    let nextId
+
+    do {
+      // Class name cannot start with a number.
+      nextId = generateNextId()
+    } while (/^[0-9_-]/.test(nextId))
+
+    index[name] = generateNextId()
+    // console.log(`${name} has id = ${index[name]}`);
+
+    return index[name]
+  }
+}
+
+const idLocal = createUniqueIdGenerator()
+const idComponent = createUniqueIdGenerator()
+const generateScopedName = (localName, resourcePath) => {
+  const componentName = resourcePath.split('/').slice(-2).join('/')
+  return idComponent(componentName).toUpperCase() + idLocal(localName)
+}
+
+const getLocalIdent = (context, _localIdentName, localName) =>
+  generateScopedName(localName, context.resourcePath)
+
+export default getLocalIdent
